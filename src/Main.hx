3c92b52c03125e244eeee4b835ee0038396f0350
Ian Swain <conanreh@gmail.com>
2015-08-29 11:29:35 -0400

    Beginning wave structures

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,36 @@
-//Maybe have like some sort of manager here, but not yet.
+import luxe.Draw;
+import luxe.Color;
+import phoenix.geometry.ArcGeometry;
 
 class Main extends luxe.Game {
-  
+
+  override function ready() : Void {
+    var radiusx = 400;
+    var radiusy = 100;
+    var pwave = Luxe.draw.arc({
+      rx: radiusx,
+      ry: radiusy,
+      start_angle: 1,
+      end_angle: -181,
+      x: radiusx,
+      y: Luxe.screen.h/2+1,
+    });
+  }
+
+  function createWave() : Array<phoenix.geometry.ArcGeometry> {
+    var returnArray : Array<phoenix.geometry.ArcGeometry> = new Array();
+    //Create large wave first
+
+
+    //Create 2 small waves on either side
+    
+
+    return returnArray;
+  }
+
+  override function config( c:luxe.AppConfig ) : luxe.AppConfig {
+    c.window.title = "waves";
+
+    return c;
+  }
 }
