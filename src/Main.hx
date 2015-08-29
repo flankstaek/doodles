ccdff0071f2b1841215934694b30b7b12096628c
Ian Swain <conanreh@gmail.com>
2015-08-29 13:02:34 -0400

    Wave function, no randomization

--- src/Main.hx
+++ src/Main.hx
@@ -1,29 +1,48 @@
 import luxe.Draw;
 import luxe.Color;
-import phoenix.geometry.ArcGeometry;
+import phoenix.geometry.CircleGeometry;
 
 class Main extends luxe.Game {
 
   override function ready() : Void {
-    var radiusx = 400;
-    var radiusy = 100;
-    var pwave = Luxe.draw.arc({
-      rx: radiusx,
-      ry: radiusy,
-      start_angle: 1,
-      end_angle: -181,
-      x: radiusx,
-      y: Luxe.screen.h/2+1,
-    });
+    createWave();
   }
 
-  function createWave() : Array<phoenix.geometry.ArcGeometry> {
-    var returnArray : Array<phoenix.geometry.ArcGeometry> = new Array();
-    //Create large wave first
+  function createWave() : Array<phoenix.geometry.CircleGeometry> {
+    var returnArray : Array<phoenix.geometry.CircleGeometry> = new Array();
 
+    //Init variables (randomize)
+    var c = new Color(1, 1, 1);
+    var rx = 400;
+    var ry = 100;
+    var x = Luxe.screen.w/2;
+    var y = Luxe.screen.h/2;
+
+    //Create large wave first
+    returnArray.push(Luxe.draw.circle({
+      rx: rx,
+      ry: ry,
+      x: x,
+      y: y,
+    }));
 
     //Create 2 small waves on either side
-    
+
+    returnArray.push(Luxe.draw.circle({
+      rx: rx/2,
+      ry: ry/2,
+      x: x - rx * 1.1, //rx * (.8, 1.1)
+      y: y + ry * .25, //ry * (0, .33)
+    }));
+
+    returnArray.push(Luxe.draw.circle({
+      rx: rx/2,
+      ry: ry/2,
+      x: x + rx * 1.1,
+      y: y + ry * .25
+    }));
+
+    //Make small waves in corners maybe
 
     return returnArray;
   }
